f9efda82024091d537c8b6c625ff60b5894d2135
Jeff L <jeff@langrsoft.com>
2024-01-22 16:41:08 -0700

    bad

--- src/main/java/util/NameNormalizer.java
+++ src/main/java/util/NameNormalizer.java
@@ -5,59 +5,35 @@ import java.util.stream.Collectors;
 
 public class NameNormalizer {
 
-   private String[] nameParts;
-   private String suffix = "";
-
-   // START:fix
+   // START:bad
    public String normalizeName(String name) {
-      extractSuffix(name);
-      // Handle case for single name with no middle names but potentially a suffix
-      // START_HIGHLIGHT
+      var extractedResult = extractSuffix(name);
+      var nameParts = extractedResult[0].split(" ");
+      var suffix = extractedResult[1];
+
       if (nameParts.length == 1) {
          return nameParts[0] + suffix;
       }
-      // END_HIGHLIGHT
 
-      return formatLastNameFirst() + suffix;
+      var middleInitials = Arrays.stream(middleNames(nameParts))
+         .map(this::initial)
+         .collect(Collectors.joining(" "));
+
+      return (nameParts[nameParts.length - 1] + ", " + nameParts[0] +
+         (middleInitials.isEmpty() ? "" : " " + middleInitials)) + suffix;
    }
-   // END:fix
+   // END:bad
 
-   private void extractSuffix(String name) {
+   private String[] extractSuffix(String name) {
       if (name.contains(",")) {
          var parts = name.split(", ", 2);
-         this.nameParts = parts[0].split(" ");
-         this.suffix = ", " + parts[1];
+         return new String[]{parts[0], ", " + parts[1]};
       } else {
-         this.nameParts = name.split(" ");
+         return new String[]{name, ""};
       }
    }
 
-   private String formatLastNameFirst() {
-      var firstName = firstName();
-      var lastName = lastName();
-      var middleInitials = middleInitialsString();
-
-      return String.format("%s, %s%s", lastName, firstName, middleInitials);
-   }
-
-   private String firstName() {
-      return nameParts[0];
-   }
-
-   private String lastName() {
-      return nameParts[nameParts.length - 1];
-   }
-
-   private String middleInitialsString() {
-      if (nameParts.length > 2) {
-         return " " + Arrays.stream(middleNames())
-            .map(this::initial)
-            .collect(Collectors.joining(" "));
-      }
-      return "";
-   }
-
-   private String[] middleNames() {
+   private String[] middleNames(String[] nameParts) {
       if (nameParts.length > 2) {
          return Arrays.copyOfRange(nameParts, 1, nameParts.length - 1);
       }
