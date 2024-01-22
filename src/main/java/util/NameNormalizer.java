2e813fa40b032e1b43a06399034f1d16fd4a90e2
Jeff L <jeff@langrsoft.com>
2024-01-22 16:46:06 -0700

    ok

--- src/main/java/util/NameNormalizer.java
+++ src/main/java/util/NameNormalizer.java
@@ -5,35 +5,54 @@ import java.util.stream.Collectors;
 
 public class NameNormalizer {
 
-   // START:bad
+   private String[] nameParts;
+   private String suffix = "";
+
+   // START:ok
    public String normalizeName(String name) {
-      var extractedResult = extractSuffix(name);
-      var nameParts = extractedResult[0].split(" ");
-      var suffix = extractedResult[1];
+      var result = extractSuffix(name);
+      this.nameParts = result[0].split(" ");
+      this.suffix = result[1];
 
       if (nameParts.length == 1) {
          return nameParts[0] + suffix;
       }
 
-      var middleInitials = Arrays.stream(middleNames(nameParts))
-         .map(this::initial)
-         .collect(Collectors.joining(" "));
-
-      return (nameParts[nameParts.length - 1] + ", " + nameParts[0] +
-         (middleInitials.isEmpty() ? "" : " " + middleInitials)) + suffix;
+      return formatLastNameFirst() + suffix;
    }
-   // END:bad
 
    private String[] extractSuffix(String name) {
       if (name.contains(",")) {
          var parts = name.split(", ", 2);
-         return new String[]{parts[0], ", " + parts[1]};
+         return new String[] {parts[0], ", " + parts[1]};
       } else {
-         return new String[]{name, ""};
+         return new String[] {name, ""};
       }
    }
+   // END:ok
 
-   private String[] middleNames(String[] nameParts) {
+   private String formatLastNameFirst() {
+      return lastName() + ", " + firstName() + middleInitialsString();
+   }
+
+   private String firstName() {
+      return nameParts[0];
+   }
+
+   private String lastName() {
+      return nameParts[nameParts.length - 1];
+   }
+
+   private String middleInitialsString() {
+      if (nameParts.length > 2) {
+         return " " + Arrays.stream(middleNames())
+            .map(this::initial)
+            .collect(Collectors.joining(" "));
+      }
+      return "";
+   }
+
+   private String[] middleNames() {
       if (nameParts.length > 2) {
          return Arrays.copyOfRange(nameParts, 1, nameParts.length - 1);
       }
